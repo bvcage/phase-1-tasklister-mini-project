556b8a19448965937d9c49076e3edd10b679914e
Bailey Cage <bailey.cage@gmail.com>
2022-08-10 19:40:34 -0400

    reimplement complete / delete functionality for table

--- src/index.js
+++ src/index.js
@@ -1,38 +1,59 @@
+TASK_ID = 1;
+
 document.addEventListener("DOMContentLoaded", () => {
   const entryForm = document.querySelector('form#create-task-form');
-  const taskList = document.querySelector('table#tasks');
+  const taskTable = document.querySelector('table#tasks');
   entryForm.addEventListener('submit', function(e){
+    
     // prevent page refresh
     e.preventDefault();
+    
     // get user input
     let inputDesc = document.querySelector('input#new-task-description');
     let inputPriority = document.querySelector('select#new-task-priority');
     let inputDueDate = document.querySelector('input#new-task-due-date');
+    
     // create row to hold task entry
-    const newTaskRow = taskList.insertRow();
-    // insert status data
-    const newTaskStatus = newTaskRow.insertCell();
+    const newTaskRow = taskTable.insertRow();
+    newTaskRow.id = `task-${TASK_ID}`;
+    newTaskRow.classList.add('task-entry-row');
+    
     // insert description data
     const newTaskDesc = newTaskRow.insertCell();
     newTaskDesc.appendChild(document.createTextNode(inputDesc.value));
+    
     // insert priority data
     const newTaskPriority = newTaskRow.insertCell();
     newTaskPriority.appendChild(document.createTextNode(inputPriority.value));
     newTaskRow.classList.add(`priority-${inputPriority.value}`);
+    
     // insert due date data
     const newTaskDueDate = newTaskRow.insertCell();
     newTaskDueDate.appendChild(document.createTextNode(inputDueDate.value));
+    
     // reset entry form
     inputDesc.value = '';
     inputPriority.value = 'none';
-    inputDueDate.value = 'none';
-
-    // // create deletion functionality
-    // newTask.setAttribute('onclick', 'deleteTask(this)');
+    inputDueDate.value = '';
     
+    // completion functionality
+    console.log(newTaskRow.cells.length);
+    for (let i = 0; i < newTaskRow.cells.length; ++i) {
+      console.log(newTaskRow.cells[i]);
+      newTaskRow.cells[i].addEventListener('click', function() {markTask(this.parentNode, 'complete')});
+    }
+    
+    // deletion functionality
+    const newTaskDelete = newTaskRow.insertCell();
+    newTaskDelete.appendChild(document.createTextNode('del'));
+    newTaskDelete.addEventListener('click', function() {markTask(this.parentNode, 'delete')});
+ 
+    // increment task id for next call
+    ++ TASK_ID;
   });
+
 });
 
-function deleteTask(ele) {
-  ele.classList.add('hide');
+function markTask(ele, marking) {
+  ele.classList.add(`${marking}`);
 }
