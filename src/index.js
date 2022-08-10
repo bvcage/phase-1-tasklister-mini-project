072bbd3e768279e6a2dfc20414e3aebee90566da
Bailey Cage <bailey.cage@gmail.com>
2022-08-10 19:01:58 -0400

    change todo list to todo table

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   const entryForm = document.querySelector('form#create-task-form');
-  const taskList = document.querySelector('ul#tasks');
+  const taskList = document.querySelector('table#tasks');
   entryForm.addEventListener('submit', function(e){
     // prevent page refresh
     e.preventDefault();
     // get user input
     let inputDesc = document.querySelector('input#new-task-description');
     let inputPriority = document.querySelector('select#new-task-priority');
-    // create element to hold task
-    const newTask = document.createElement('li');
-    // assign user input to task element
-    newTask.textContent = inputDesc.value;
-    newTask.classList.add(`priority-${inputPriority.value}`);
-    // create deletion functionality
-    newTask.setAttribute('onclick', 'deleteTask(this)');
-    // add new task element to task list
-    taskList.appendChild(newTask);
+    let inputDueDate = document.querySelector('input#new-task-due-date');
+    // create row to hold task entry
+    const newTaskRow = taskList.insertRow();
+    // insert status data
+    const newTaskStatus = newTaskRow.insertCell();
+    // insert description data
+    const newTaskDesc = newTaskRow.insertCell();
+    newTaskDesc.appendChild(document.createTextNode(inputDesc.value));
+    // insert priority data
+    const newTaskPriority = newTaskRow.insertCell();
+    newTaskPriority.appendChild(document.createTextNode(inputPriority.value));
+    newTaskRow.classList.add(`priority-${inputPriority.value}`);
+    // insert due date data
+    const newTaskDueDate = newTaskRow.insertCell();
+    newTaskDueDate.appendChild(document.createTextNode(inputDueDate.value));
     // reset entry form
     inputDesc.value = '';
     inputPriority.value = 'none';
+    inputDueDate.value = 'none';
+
+    // // create deletion functionality
+    // newTask.setAttribute('onclick', 'deleteTask(this)');
+    
   });
 });
 
