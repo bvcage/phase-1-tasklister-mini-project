e8a01d325f44055305b5dc17cf611364c3b8b0fe
Bailey Cage <bailey.cage@gmail.com>
2022-08-10 20:09:58 -0400

    format task table

--- src/index.js
+++ src/index.js
@@ -17,19 +17,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const newTaskRow = taskTable.insertRow();
     newTaskRow.id = `task-${TASK_ID}`;
     newTaskRow.classList.add('task-entry-row');
+
+    // insert cell for checkmark
+    const newTaskStatus = newTaskRow.insertCell();
+    newTaskStatus.classList.add('col-status');
     
     // insert description data
     const newTaskDesc = newTaskRow.insertCell();
     newTaskDesc.appendChild(document.createTextNode(inputDesc.value));
+    newTaskDesc.classList.add('col-desc');
     
     // insert priority data
     const newTaskPriority = newTaskRow.insertCell();
     newTaskPriority.appendChild(document.createTextNode(inputPriority.value));
+    newTaskPriority.classList.add('col-priority');
     newTaskRow.classList.add(`priority-${inputPriority.value}`);
     
     // insert due date data
     const newTaskDueDate = newTaskRow.insertCell();
     newTaskDueDate.appendChild(document.createTextNode(inputDueDate.value));
+    newTaskDueDate.classList.add('col-date');
     
     // reset entry form
     inputDesc.value = '';
